a7ea5f6ce7ff02d4c89e16ed36202f37b3a6f2a0
liuqing.yang <liuqing.yang@mobvoi.com>
2018-12-10 20:07:17 +0800

    nio convert

--- netty_lecture/src/main/java/com/shengsiyuan/nio/NioTest.java
+++ netty_lecture/src/main/java/com/shengsiyuan/nio/NioTest.java
@@ -1,7 +1,10 @@
 package com.shengsiyuan.nio;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.IntBuffer;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class NioTest {
@@ -13,13 +16,38 @@ public class NioTest {
             SecureRandom sr = new SecureRandom();
             return sr.nextInt(20);
         })
-              .limit(intBuffer.capacity())
-              .forEach(intBuffer::put); // write
+                .limit(intBuffer.capacity())
+                .forEach(intBuffer::put); // write
 
         intBuffer.flip(); // 状态反转
 
         while (intBuffer.hasRemaining()) {
             System.out.println(intBuffer.get()); // read
         }
+
+        intBuffer.flip();
+        System.out.println("=============");
+
+        while (intBuffer.hasRemaining()) {
+            System.out.println(intBuffer.get()); // read
+        }
+
+        System.out.println("=================");
+
+        byte[] ba = {1, 2, 3, 4};
+        // 4 bytes to int
+        int in = ByteBuffer.wrap(ba).order(ByteOrder.LITTLE_ENDIAN).getInt();
+        System.out.println(in);
+
+
+        byte[] ab = null;
+        // int to 4 bytes method 1
+        ab = ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(in).array();
+        System.out.println(Arrays.toString(ab));
+
+        // int to 4 bytes method 2
+        ab = new byte[4];
+        ByteBuffer.wrap(ab).order(ByteOrder.LITTLE_ENDIAN).asIntBuffer().put(in);
+        System.out.println(Arrays.toString(ab));
     }
 }
