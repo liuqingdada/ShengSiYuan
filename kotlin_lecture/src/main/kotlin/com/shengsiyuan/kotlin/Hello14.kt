ac6fc0c87f620eba47ddd3d037d459190f6a3e36
suhen <1239604859@qq.com>
2018-12-11 00:02:55 +0800

    finish kotlin 22

--- kotlin_lecture/src/main/kotlin/com/shengsiyuan/kotlin/Hello14.kt
+++ kotlin_lecture/src/main/kotlin/com/shengsiyuan/kotlin/Hello14.kt
@@ -4,10 +4,10 @@ package com.shengsiyuan.kotlin
  * Created by liuqing
  * 2018/11/8.
  * Email: suhen0420@163.com
+ *
+ * 所有类在默认情况下都无法被继承
+ * 换句话说，所有类默认情况下都是final的
  */
-
-// 所有类在默认情况下都无法被继承
-// 换句话说，所有类默认情况下都是final的
 
 open class Parent(name: String, age: Int) {
 
--- kotlin_lecture/src/main/kotlin/com/shengsiyuan/kotlin3/Test11.kt
+++ kotlin_lecture/src/main/kotlin/com/shengsiyuan/kotlin3/Test11.kt
@@ -1,7 +1,50 @@
 package com.shengsiyuan.kotlin3
 
+import java.awt.event.WindowEvent
+import java.awt.event.WindowListener
+
 /**
  * Created by suhen
  * 18-12-10.
  * Email: 1239604859@qq.com
+ *
+ * 对象表达式是立刻初始化或是执行的
+ * 对象声明是延迟初始化的, 在首次访问的时候进行
+ * 伴生对象是在其所在的类被加载的时候初始化的, 对应于Java的静态初始化
  */
+
+object MyObject {
+    fun method() = "hello world"
+}
+
+object MyObject2 : WindowListener {
+    override fun windowDeiconified(e: WindowEvent?) {
+    }
+
+    override fun windowClosing(e: WindowEvent?) {
+    }
+
+    override fun windowClosed(e: WindowEvent?) {
+    }
+
+    override fun windowActivated(e: WindowEvent?) {
+    }
+
+    override fun windowDeactivated(e: WindowEvent?) {
+    }
+
+    override fun windowOpened(e: WindowEvent?) {
+    }
+
+    override fun windowIconified(e: WindowEvent?) {
+    }
+
+    fun method() = "welcome"
+}
+
+fun main() {
+    println(MyObject.method())
+
+    val obj = MyObject2
+    println(obj.method())
+}
