ae1b9553824293271dced5a7883503bbacec299a
liuqingdada <1239604859@qq.com>
2019-08-19 01:51:09 +0800

    rebuild kotlin project

---
+++ kotlin/coroutin/src/main/kotlin/com/shengsiyuan/coroutine/CoroutinTest.kt
@@ -0,0 +1,23 @@
+package com.shengsiyuan.coroutine
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * Created by yangliuqing
+ * 2019-08-11.
+ * Email: 1239604859@qq.com
+ */
+fun main() {
+    GlobalScope.launch {
+        delay(1000)
+        println("Kotlin Coroutine: ${Thread.currentThread().name}")
+    }
+
+    println("Hello: ${Thread.currentThread().name}")
+
+    Thread.sleep(2000)
+
+    println("World: ${Thread.currentThread().name}")
+}
---
+++ kotlin/coroutin/src/main/kotlin/com/shengsiyuan/coroutine/CoroutinTest2.kt
@@ -0,0 +1,21 @@
+package com.shengsiyuan.coroutine
+
+import kotlin.concurrent.thread
+
+/**
+ * Created by yangliuqing
+ * 2019-08-11.
+ * Email: 1239604859@qq.com
+ */
+fun main() {
+    thread {
+        Thread.sleep(1000)
+        println("Kotlin c thread: ${Thread.currentThread().name}")
+    }
+
+    println("Hello")
+
+    Thread.sleep(2000)
+
+    println("World")
+}
