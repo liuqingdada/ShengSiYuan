edafae62bd755240d097d51811008e22fe804e9b
liuqing.yang <myangliuqing@gmail.com>
2020-05-17 22:26:17 +0800

    Spring: finish 4

--- spring_lecture/core/src/main/java/com/shengsiyuan/spring/SpringClient.java
+++ spring_lecture/core/src/main/java/com/shengsiyuan/spring/SpringClient.java
@@ -2,6 +2,7 @@ package com.shengsiyuan.spring;
 
 import com.shengsiyuan.spring.bean.Student;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionReader;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
@@ -24,6 +25,20 @@ import org.springframework.core.io.Resource;
  * Dependency Injection 依赖注入
  * A 包含属性 B，也就是 A 依赖 B
  * A 创建的时候，B 也要创建好，并且注入到 A
+ * <p>
+ * 总结:
+ * 1. bean信息定义在 spring 的配置文件中
+ * 2. spring 抽象出来的 resource 来指定对应的配置文件
+ * 3. 声明一个 spring 工厂，用来掌握配置文件中声明的各种 bean，以及 bean 之间的依赖关系与注入关系
+ * 4. 定义一个配置信息读取器，该读取器用来读取之前所定义的 bean 配置文件信息
+ * 5. 读取器的作用是读取我们所声明的配置文件信息，并且将读取后的信息装配到之前所声明的工厂中
+ * 6. 读取器、工厂、资源对象进行相应的关联处理
+ * 7. 工厂所管理的全部对象装配完毕，可以供客户端直接调用，获取客户端要使用的各种 bean 对象
+ * <p>
+ * spring 对应 bean 管理的核心组件:
+ * 资源抽象、工厂、配置信息读取器
+ * <p>
+ * {@link BeanFactory}是 spring bean 工厂的最顶层的抽象
  */
 public class SpringClient {
     public static void main(String[] args) {
